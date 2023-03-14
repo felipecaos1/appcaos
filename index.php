df1bbffdedd5a7d52fd3b6ae878dd67ae9710d20
felipecaos1 <andresfelipecao@gmail.com>
2023-03-13 21:11:18 -0500

    Creacion del index.php y seccionar en layouts

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+require_once 'autoload.php';
+require_once 'config/db.php';
+require_once 'config/parameters.php'; 
+
+require_once 'views/layouts/header.php'; 
+require_once 'views/layouts/sidebar.php'; 
+        
+
+            
+require_once 'views/layouts/topbar.php';
+
+
+
+
+require_once 'views/layouts/page.php' ;
+require_once 'views/layouts/footer.php' ;
